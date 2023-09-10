1ff2068a4d423a09f2f07ff1226fb6c97d98bd6c
xxbaizero0 <xxbaizero@gmail.com>
2023-09-10 23:19:45 +0800

    Fri 9/8

--- lab/disc02/disc02.py
+++ lab/disc02/disc02.py
@@ -25,88 +25,24 @@ make_keeper(5)(lambda x: True)
     # 5
 make_keeper(5)(lambda x: False) # Nothing is printed
 
-'''Q6: Currying'''
-def curry(func):
-    """
-    Returns a Curried version of a two-argument function FUNC.
-    >>> from operator import add, mul, mod
-    >>> curried_add = curry(add)
-    >>> add_three = curried_add(3)
-    >>> add_three(5)
-    8
-    >>> curried_mul = curry(mul)
-    >>> mul_5 = curried_mul(5)
-    >>> mul_5(42)
-    210
-    >>> curry(mod)(123)(10)
-    3
-    """
-    "*** YOUR CODE HERE ***"
 
-
-
-'''Q7: Make Your Own Lambdas'''
-def f1():
-    """
-    >>> f1()
-    3
-    """
-    "*** YOUR CODE HERE ***"
-def f2():
-    """
-    >>> f2()()
-    3
-    """
-    "*** YOUR CODE HERE ***"
-def f3():
-    """
-    >>> f3()(3)
-    3
-    """
-    "*** YOUR CODE HERE ***"
-def f4():
-    """
-    >>> f4()()(3)()
-    3
-    """
-    "*** YOUR CODE HERE ***"
-# You can use more space on the back if you want
-
-
-'''Q8: Match Maker
-Implement match_k, which takes in an integer k and returns a function that takes in a variable x and returns True
-if all the digits in x that are k apart are the same.
-For example, match_k(2) returns a one argument function that takes in x and checks if digits that are 2 away in x
-are the same.
-match_k(2)(1010) has the value of x = 1010 and digits 1, 0, 1, 0 going from left to right. 1 == 1 and 0 == 0, so
-the match_k(2)(1010) results in True.
-match_k(2)(2010) has the value of x = 2010 and digits 2, 0, 1, 0 going from left to right. 2 != 1 and 0 == 0, so
-the match_k(2)(2010) results in False.
-Important: You may not use strings or indexing for this problem. You do not have to use all the lines; one staff
-solution does not use the line directly above the while loop.
-Hint: Floor dividing by powers of 10 gets rid of the rightmost digits.'''
 def match_k(k):
-    """Returns a function that checks if digits k apart match.
-    >>> match_k(2)(1010)
-    True
-    >>> match_k(2)(2010)
-    False
-    >>> match_k(1)(1010)
-    False
-    >>> match_k(1)(1)
-    True
-    >>> match_k(1)(2111111111111111)
-    False
-    >>> match_k(3)(123123)
-    True
-    >>> match_k(2)(123123)
-    False
-    """
-def ____________________________:
-____________________________
-while ____________________________:
-if ____________________________:
-return ____________________________
-____________________________
-____________________________
-____________________________
+    def apart(n):
+        if k == 1 and n ==1:
+            return True
+        part_1, part_2 = n % (10 ** k), n // (10 ** k)
+        return part_1 == part_2
+    return apart
+print(match_k(2)(1010))
+print(match_k(2)(2010))
+print(match_k(1)(1010))
+print(match_k(1)(1))
+print(match_k(1)(2111111111111111))
+
+def f4():
+    # f4()()(3)()
+    # 3
+    return lambda :lambda x:lambda :x
+print(f4()()(3)())
+
+
