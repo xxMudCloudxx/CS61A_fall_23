89a79666266b0507417d591c33a335f7e44bd8aa
xxbaizero0 <142685411+xxbaizero0@users.noreply.github.com>
2023-09-19 10:48:30 +0800

    更新 disc03.py

--- lab/disc03/disc03.py
+++ lab/disc03/disc03.py
@@ -95,6 +95,6 @@ def merge(n1, n2):
     elif n2 == 0:
         return n2
     elif n1%10 > n2%10:
-        return n1%10*10+n2%2+merge(n1//10,n2//10)
+        return merge(n1,n2//10)*10+n2%10
     else:
-        return n2%10*10+n1%2+merge(n1//10,n2//10)
+        return merge(n1//10,n2)*10+n1%10
